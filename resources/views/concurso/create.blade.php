beabfd705e260e6372a3403e285e51a20b6f1a16
Joao Vitor Aguiar <joaovitoraguiarsilva@gmail.com>
2022-10-25 19:09:51 -0300

    :sparkles: Adicionando novo layout aos cards

--- resources/views/concurso/create.blade.php
+++ resources/views/concurso/create.blade.php
@@ -1,25 +1,42 @@
 <x-layout tituloPagina="Novo Concurso">
     <div>
         <div>
-            <h1 class="display-6">
+            <h1 class="display-4">
                 Criar novo concurso
             </h1>
         </div>        
         <div class="card bg-dark text-white">
-            <div class="card-body">
-                <h5 class="card-title">Novo Concurso</h5>                    
-                <form >
-                    <div class="row">
+            <div class="card-body">                  
+                <form>
+                    <div class="row">                        
+                        <h1 class="display-6">Concurso</h1>  
                         <div class="col-8">
                             <label for="" class="form-label">Nome Concuros</label>
                             <input class="form-control" type="text" name="nome" id="">
                         </div>
                         <div class="col-4">
-                            <label for="" class="form-label">Data da Prova</label>
-                            <input class="form-control" type="date" name="dt_prova" id="">
+                            <label for="" class="form-label">Status Concurso</label>
+                            <input class="form-control" type="text" name="status_concurso" id="">
                         </div>
                         <div class="col-6">
-                            <label for="" class="form-label">Perfl do Concurso</label>                        
+                            <label for="" class="form-label">Sigla Estado do curso</label>
+                            <input class="form-control" type="text" name="status_concurso" id="">                           
+                        </div>   
+                        <div class="col-6">
+                            <label for="" class="form-label" >Custo de inscrição</label>
+                            <input type="number" class="form-control" name="valor_prova">
+                        </div>                        
+                        <div class="col-6">
+                            <label for="" class="form-label">Status da comissão</label>
+                            <select class="form-select" aria-label="Default select example" name="comissao">
+                                @foreach(['Formada', 'Em formação'] as $cont => $comissao)
+                                    <option value="{{ $cont }}">{{ $comissao }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <h1 class="display-6" >Cargos do concurso</h1>
+                        <div class="col-6">
+                            <label for="" class="form-label">Perfl do Concurso</label>                     
                             <select class="form-select" aria-label="Default select example" name="perfil" >
                                 @foreach(['Judiciario', 'Adminstrativo', 'Controle', 'Segurança', 'Fiscal', 'Lesgislativa', 'Alto Gestão', 'Magistério', 'Militar', 'Bancária'] as $perfilConcurso)
                                 <option value="{{ $perfilConcurso }}">{{ $perfilConcurso }}</option>
@@ -27,25 +44,13 @@
                             </select>
                         </div>
                         <div class="col-6">
-                            <label for="" class="form-label">Perfl do Concurso</label>
-                            <select class="form-select" aria-label="Default select example" name="tipo" >
-                                @foreach(['distrital', 'municipal', 'estadual', 'federal'] as $tipoConcurso)
-                                    <option value="{{ $tipoConcurso }}">{{ $tipoConcurso }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-4">
-                            <label for="" class="form-label">Salário do cargo do concurso</label>
+                            <label for="" class="form-label">Salário cargo</label>
                             <input class="form-control" type="number" name="salario">
                         </div>  
-                        <div class="col-4">
+                        <div class="col-6">
                             <label for="" class="form-label">Quantidade de vagas</label>
                             <input class="form-control" type="number" name="salario">
                         </div>  
-                        <div class="col-4">
-                            <label for="" class="form-label" >Custo de inscrição</label>
-                            <input type="number" class="form-control" name="valor_prova">
-                        </div>   
                         <div class="col-6">
                             <label for="" class="form-label">Escolaridade</label>
                             <select class="form-select" aria-label="Default select example" name="ensino" >
@@ -56,16 +61,8 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="col-6">
-                            <label for="" class="form-label">Estado da comissão</label>
-                            <select class="form-select" aria-label="Default select example" name="comissao">
-                                @foreach(['Formada', 'Em formação'] as $cont => $comissao)
-                                    <option value="{{ $cont }}">{{ $comissao }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div>
-                            <label for="" class="form-label">Descrição Concuros</label>
+                        <div class="col-12">
+                            <label for="" class="form-label">Descrição Cargo</label>
                             <textarea name="descricao" class="form-control" id="" cols="30" rows="10"></textarea>
                         </div>
                         <div>
--- resources/views/noticias/index.blade.php
+++ resources/views/noticias/index.blade.php
@@ -1,14 +1,18 @@
 <x-layout tituloPagina="Tudo Concurso Noticias">
+  <section class="row"></section>
   @foreach($topNoticias as $noticia)
-  <div class="card bg-dark text-white m-2 flex-row border border-0 p-3">
-    <img class="card-img  w-25" src=" {{ $noticia['urlToImage'] }}" alt="Card image">
-    <div class="card-body">
-      <h5 class="card-title">{{ $noticia['title'] }}</h5>
-      <p class="card-text">{{ $noticia['description'] }}</p>
-      <p class="card-text">{{ $noticia['url'] }}</p>
-      <p class="card-text">{{ $noticia['publishedAt'] }}</p>
+  <a class="card-text decoration-none" href="{{ $noticia['url'] }}">
+    <div class="card bg-dark text-white m-2 flex-row border border-0 p-3">
+      <img class="card-img card-image" src=" {{ $noticia['urlToImage'] }}" alt="Card image">
+      <div class="card-body">
+        <h5 class="card-title">{{ $noticia['title'] }}</h5>
+        <p class="card-text">{{ $noticia['description'] }}</p>
+        <div class="d-flex align-items-end">
+          <p class="card-text m-0">{{ $noticia['publishedAt'] }}</p>
+        </div>
+      </div>
     </div>
-  </div>
+  </a>
   @endforeach
 </x-layout>
 
